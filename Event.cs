4034b1eadafb75e53a1b7bc8601ff5280a1b0f01
Logan Taveira <ltaveir1@binghamton.edu>
2020-02-08 16:34:01 -0500

    Added Event class and Tester class for testing Event.

---
+++ Event.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class Event
+{
+	public String name;
+	public String location;
+	public DateTime startTime;
+	public DateTime endTime;
+	public Boolean[] days;
+
+	public Event(String name, String location, DateTime startTime, DateTime endTime, Boolean[] days)
+	{
+		this.name = name;
+		this.location = location;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.days = days;
+	}
+	//setters
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setLocation(String location)
+	{
+		this.location = location;
+	}
+	public void setStartTime(DateTime startTime)
+	{
+		this.startTime = startTime;
+	}
+	public void setEndTime(DateTime endTime)
+	{
+		this.endTime = endTime;
+	}
+	public void setDay(Boolean[] days)
+	{
+		this.days = days;
+	}
+	//getters
+	public String getName()
+	{
+		return name;
+	}
+	public String getLocation()
+	{
+		return location;
+	}
+	public DateTime setStartTime()
+	{
+		return startTime;
+	}
+	public DateTime setEndTime()
+	{
+		return endTime;
+	}
+	public Boolean[] setDay()
+	{
+		return days;
+	}
+}
---
+++ Tester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Tester
+{
+	public Tester()
+	{
+
+	}
+
+	public static void Main(String[] args)
+    {
+		String name = "Math";
+		String location = "Here";
+		DateTime start = new DateTime(08,15,2001, 10,40);
+		DateTime end = new DateTime(08, 15, 2001, 11, 30);
+		Boolean[] days = { true, false, true, false, true, false, true };
+		Event g = new Event(name, location, start, end, days);
+		System.Console.WriteLine(g.getName());
+    }
+}
