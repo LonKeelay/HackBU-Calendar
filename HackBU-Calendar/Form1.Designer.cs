2093969767e4365d3ad99a904c7bd4f61e43f3cb
lonkeelay <lonkeelay@gmail.com>
2020-02-09 11:54:11 -0500

    UI Changes

--- HackBU-Calendar/Form1.Designer.cs
+++ HackBU-Calendar/Form1.Designer.cs
@@ -28,9 +28,10 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.datStart = new System.Windows.Forms.DateTimePicker();
@@ -235,16 +236,16 @@
             // deChart
             // 
             this.deChart.BackColor = System.Drawing.Color.Transparent;
-            chartArea2.Name = "ChartArea1";
-            this.deChart.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.deChart.Legends.Add(legend2);
+            chartArea1.Name = "ChartArea1";
+            this.deChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.deChart.Legends.Add(legend1);
             this.deChart.Location = new System.Drawing.Point(193, 12);
             this.deChart.Name = "deChart";
-            series2.ChartArea = "ChartArea1";
-            series2.Legend = "Legend1";
-            series2.Name = "Series1";
-            this.deChart.Series.Add(series2);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.deChart.Series.Add(series1);
             this.deChart.Size = new System.Drawing.Size(679, 416);
             this.deChart.TabIndex = 19;
             this.deChart.Text = "chart1";
@@ -298,6 +299,7 @@
             this.Controls.Add(this.datStart);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Google Calendar - HackBU 2020";
             this.Load += new System.EventHandler(this.Form1_Load);
